f136b9b130ac12bed63eba00929ff40b64e5b7bc
kvpratama <kvpratama@gmail.com>
2025-05-20 18:06:05 +0900

    Update agent.py

--- agent.py
+++ agent.py
@@ -8,7 +8,7 @@ from langchain_google_genai import ChatGoogleGenerativeAI
 from langgraph.checkpoint.memory import MemorySaver
 import uuid
 import logging
-from tools import wikipedia_search_html, website_scrape, web_search
+from tools import wikipedia_search_html, website_scrape, web_search, visual_model, audio_model, run_python, data_tool
 import os
 
 
@@ -42,7 +42,7 @@ def get_graph():
     )
 
     # Define tools
-    tools = [web_search, wikipedia_search_html, website_scrape]
+    tools = [web_search, wikipedia_search_html, website_scrape, visual_model, audio_model, run_python, data_tool]
     
     # Bind tools to the LLM
     chat_with_tools = llm.bind_tools(tools)
@@ -50,9 +50,9 @@ def get_graph():
     # Define the state type with annotations
     class AgentState(TypedDict):
         messages: Annotated[list[AnyMessage], add_messages]
-        last_ai_message: Optional[str]
+        # last_ai_message: Optional[str]
         human_message: Optional[str]
-        planner_message: Optional[str]
+        # planner_message: Optional[str]
         error: Optional[str]  # Added error field for tracking issues
 
     # Initialize the state
@@ -201,7 +201,7 @@ class BasicAgent:
         self.config = {"configurable": {"thread_id": thread_id}}
         logger.info(f"Agent thread ID: {thread_id}")
 
-    def __call__(self, question: str) -> str:
+    def __call__(self, question: str, task_id: str) -> str:
         """Process a question through the agent and return the response."""
         logger.info(f"Agent received question: {question[:50]}...")
         # Create a system message to guide the model's behavior
@@ -210,9 +210,10 @@ class BasicAgent:
         )
         try:
             # Construct initial state
+            question += f"\n task_id={task_id}"
             initial_state = {
                 "messages": [system_message, HumanMessage(content=question)],
-                "last_ai_message": None,
+                # "last_ai_message": None,
                 "human_message": None,
                 "error": None
             }
@@ -242,6 +243,20 @@ if __name__ == "__main__":
     agent = BasicAgent()
     # response = agent("How many studio albums were published by Mercedes Sosa between 2000 and 2009 (included)? You can use the latest 2022 version of english wikipedia.")
     # response = agent('.rewsna eht sa "tfel" drow eht fo etisoppo eht etirw ,ecnetnes siht dnatsrednu uoy fI')
-    response = agent("Who nominated the only Featured Article on English Wikipedia about a dinosaur that was promoted in November 2016?")
+    # response = agent("Who nominated the only Featured Article on English Wikipedia about a dinosaur that was promoted in November 2016?")
+    # response = agent({"question": "Review the chess position provided in the image. It is black's turn. Provide the correct next move for black which guarantees a win. Please provide your response in algebraic notation.", "task_id": "cca530fc-4052-43b2-b130-b30968d8aa44"})
+    # response = agent(question="What is the surname of the equine veterinarian mentioned in 1.E Exercises from the chemistry materials licensed by Marisa Alviar-Agnew & Henry Agnew under the CK-12 license in LibreText's Introductory Chemistry materials as compiled 08/21/2023?", task_id="cabe07ed-9eca-40ea-8ead-410ef5e83f91")
+    # response = agent(question="""Hi, I'm making a pie but I could use some help with my shopping list. I have everything I need for the crust, but I'm not sure about the filling. I got the recipe from my friend Aditi, but she left it as a voice memo and the speaker on my phone is buzzing so I can't quite make out what she's saying. Could you please listen to the recipe and list all of the ingredients that my friend described? I only want the ingredients for the filling, as I have everything I need to make my favorite pie crust. I've attached the recipe as Strawberry pie.mp3.
+
+    # In your response, please only list the ingredients, not any measurements. So if the recipe calls for "a pinch of salt" or "two cups of ripe strawberries" the ingredients on the list would be "salt" and "ripe strawberries".
+
+    # Please format your response as a comma separated list of ingredients. Also, please alphabetize the ingredients.task_id=99c9cc74-fdc8-46c6-8f8d-3ce2d3bfeea3""", task_id="99c9cc74-fdc8-46c6-8f8d-3ce2d3bfeea3")
+    # response = agent(question="""Hi, I was out sick from my classes on Friday, so I'm trying to figure out what I need to study for my Calculus mid-term next week. My friend from class sent me an audio recording of Professor Willowbrook giving out the recommended reading for the test, but my headphones are broken :(
+
+    # Could you please listen to the recording for me and tell me the page numbers I'm supposed to go over? I've attached a file called Homework.mp3 that has the recording. Please provide just the page numbers as a comma-delimited list. And please provide the list in ascending order. task_id=1f975693-876d-457b-a649-393859e79bf3""", task_id="1f975693-876d-457b-a649-393859e79bf3")
+
+    # response = agent(question="""What is the final numeric output from the attached Python code?task_id=f918266a-b3e0-4914-865d-4faa564f1aef""", task_id="f918266a-b3e0-4914-865d-4faa564f1aef")
+
+    response = agent(question="""The attached Excel file contains the sales of menu items for a local fast-food chain. What were the total sales that the chain made from food (not including drinks)? Express your answer in USD with two decimal places.task_id=7bd855d8-463d-4ed5-93ca-5fe35145f733""", task_id="7bd855d8-463d-4ed5-93ca-5fe35145f733")
     
     print(f"Response: {response}")
