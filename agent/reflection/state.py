db46eb52cca718ea8818f7344a01ecc3b0e15ffb
kvpratama <kvpratama@gmail.com>
2025-06-27 16:37:51 +0900

    Reflection Agent

---
+++ agent/reflection/state.py
@@ -0,0 +1,19 @@
+from typing import Optional
+from langgraph.graph import MessagesState
+
+# Define the state type with annotations
+class AgentState(MessagesState):
+    system_message: str
+    last_ai_message: str
+    question: str
+    final_answer: str
+    ready_to_answer: bool
+    error: Optional[str]
+
+class AgentStateInput(MessagesState):
+    system_message: str
+    question: str
+
+class AgentStateOutput(MessagesState):
+    final_answer: str
+    error: Optional[str]
